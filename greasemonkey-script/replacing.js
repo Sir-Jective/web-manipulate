2aeafd9ff9c78c67aaaf101654e8e53f43d14622
Sir-Jective <Sir-Jective@users.noreply.github.com>
2017-07-18 18:07:53 +0200

    Update replacing.js

--- greasemonkey-script/replacing.js
+++ greasemonkey-script/replacing.js
@@ -1,4 +1,10 @@
-function toappend(){
+function replacement(){
   //content to be the replacement of some script.
-  alert("I was injected!");
+  alert("I was injected! I replaced the script_to_be_replaced.js");
 }
+
+function additional_script(){
+  //content to be executed after script_to_be_replaced.js
+  alert("I was injected! I am executing after script_after_which_insert.js");
+}
+
